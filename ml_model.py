cbabcece379bd437c395d5def77bbf94ecc04647
AakashMal11 <aakash.mal9133@gmail.com>
2023-01-29 18:56:55 +0530

    Final Ml model is been decided

---
+++ ml_model.py
@@ -0,0 +1,13 @@
+
+print('Adding all ML related Code here!')
+
+def model1():
+    print("this is model 1 code")
+
+def model2():
+    print("this is model 2 code")
+
+def model3():
+    print("this is model 3 code")
+
+print("Model 2 is the best performing code")
